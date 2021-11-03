a5893026c8d3f3afce233708ab93f0dc52ee315a
Josep <stoutxuzzo@gmail.com>
2021-11-03 09:07:23 +0100

    First commit

---
+++ module1/importActivity.py
@@ -0,0 +1,16 @@
+import extra.good.best.sigma as s
+import extra.good.best.tau as t
+import extra.good.alpha as a
+import extra.good.beta as b
+import extra.ugly.omega as o
+import extra.ugly.psi as p
+import extra.iota as i
+
+
+print(a.FunA())
+print(b.FunB())
+print(i.FunI())
+print(o.FunO())
+print(p.FunP())
+print(s.FunS())
+print(t.FunT())
---
+++ module1/importMath.py
@@ -0,0 +1,15 @@
+
+from math import pow, ceil, floor, pi # I can import only the things (functions and constants) 
+#i need to use
+
+print(pow(2,3)) # When you import using this tecnique, you don't need to put 
+# the name of the imported module
+
+import math
+
+# On an imported class, the functios are marct as cubes and the constants as a blue "corchetes".
+
+print(math.pow(2,3)) # This is a function, it has "parentesis".
+
+print(math.pi) # This is a constant, it doesn't have "parentesis".
+
